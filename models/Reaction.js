77e6995e2b242959616682b5d62597be2cb77add
Edgar Vargas <vargased23@gmail.com>
2023-04-05 22:07:56 -0700

    updated reaction,thought,user and index.js in models folder

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,4 +1,33 @@
-const User = require('./User');
-const Thought = require('./Thought');
+const { Schema, Types } = require('mongoose');
+const dayjs = require('dayjs')
 
-module.exports = { User, Thought };
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: () => dayjs().format("dddd, MMMM D YYYY")
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+module.exports = reactionSchema;
